808d56b4d50113a8d2e3b980b7289d7814ecf0a7
B00130589 <75186722+B00130589@users.noreply.github.com>
2020-11-29 05:05:17 +0000

    UI added

---
+++ alpha1/Assets/Scripts/ChangeScene.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ChangeScene : MonoBehaviour
+{
+    public string URL = "https://github.com/B00130589/alpha1";
+   public void GoToScne1()
+    {
+        SceneManager.LoadScene("scene1");
+    }
+    public void GoToSene2()
+    {
+        SceneManager.LoadScene("scene2");
+    }
+
+    public void LoadUrl()
+    {
+        Application.OpenURL(URL);
+    }
+}
